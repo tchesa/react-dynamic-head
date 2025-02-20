a014f28e48acd52b6ece90b13eada9e3cf6643bf
cesarlima-plank <cesar.lima@joinplank.com>
2025-02-20 10:44:56 -0300

    push routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Hello from "./pages/hello.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/hello" element={<Hello />} />
+    </Routes>
+  </BrowserRouter>
+);
